ff9e33b443d2b374509d5c112fe9a7e3d322dbff
mikeremerscheid <mikeremerscheid@gmail.com>
2024-05-25 23:16:37 -0400

    getting started with this server creation projecdt

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Server { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
