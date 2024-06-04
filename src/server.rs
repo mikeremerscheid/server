0acb2c27081283292b9d5b9b53e94b1038ccafdd
mikeremerscheid <mikeremerscheid@gmail.com>
2024-06-03 23:16:22 -0400

    Added match expression to handle different Results.

--- src/server.rs
+++ src/server.rs
@@ -12,9 +12,28 @@ impl Server {
 
     pub fn run(self) {
 
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept(){
+                Ok((stream, _)) => {
+                    let a = 5;
+                    println!("OK");
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+
+            }
+
+            let res = listener.accept();
+
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
+        }
     }
 }
 
